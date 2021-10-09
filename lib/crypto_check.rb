3071c11b1093b099981123d390b71cc3f10dc7fc
Chris Gannon <cjgannon@gmail.com>
2021-10-09 17:16:00 +0100

    Add method for fetching selected information only for selected cryptocurrencies

--- lib/crypto_check.rb
+++ lib/crypto_check.rb
@@ -5,9 +5,26 @@ require 'net/http'
 require 'json'
 
 class CryptoCheck
-  def self.fetch_cryptocurrencies_information(requested_crypto_currencies)
-    uri = URI("https://api.nomics.com/v1/currencies/ticker?key=#{ENV['NOMICS_API_KEY']}&ids=#{requested_crypto_currencies.join(',')}")
+  BASE_URI = "https://api.nomics.com/v1/currencies/ticker?key=#{ENV['NOMICS_API_KEY']}".freeze
+
+  def self.fetch_cryptocurrencies_information(crypto_currencies)
+    uri = URI("#{BASE_URI}&ids=#{crypto_currencies.join(',')}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
+  end
+
+  def self.fetch_selected_information_for_cryptocurrencies(crypto_currencies: [], attributes: [])
+    attributes << "id"
+    uri = URI("#{BASE_URI}&ids=#{crypto_currencies.join(',')}&attributes=#{attributes.join(',')}")
+    response = Net::HTTP.get(uri)
+    parsed_response = JSON.parse(response)
+
+    parsed_response.map do |hash|
+      {}.tap do |new_hash|
+        attributes.each do |key|
+          new_hash[key] = hash[key]
+        end
+      end
+    end
   end
 end
--- spec/lib/crypto_check_spec.rb
+++ spec/lib/crypto_check_spec.rb
@@ -1,21 +1,38 @@
-RSpec.describe CryptoCheck, "#fetch_cryptocurrencies_information" do
-  context 'for one cryptocurrency' do
-    it 'will provide the payload for the requested cryptocurrency', :vcr do
-      payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC'])
+RSpec.describe CryptoCheck do
+  describe "#fetch_cryptocurrencies_information" do
+    context 'for one cryptocurrency' do
+      it 'will provide the payload for the requested cryptocurrency', :vcr do
+        payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC'])
 
-      expect(payload.first["id"]).to eq("BTC")
+        expect(payload.first["id"]).to eq("BTC")
+      end
+
+      it 'will provide the full payload for the requested cryptocurrency', :vcr do
+        payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC'])
+
+        expect(payload.first.keys).to match_array(["id", "currency", "symbol", "name", "logo_url", "status", "price", "price_date", "price_timestamp", "circulating_supply", "max_supply", "market_cap", "market_cap_dominance", "num_exchanges", "num_pairs", "num_pairs_unmapped", "first_candle", "first_trade", "first_order_book", "rank", "rank_delta", "high", "high_timestamp", "1d", "7d", "30d", "365d", "ytd"])
+      end
     end
 
-    it 'will provide the full payload for the requested cryptocurrency', :vcr do
-      payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC'])
+    context 'for multiple cryptocurrencies' do
+      it 'will provide the payload for the requested cryptocurrency', :vcr do
+        payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC', 'XRP', 'ETH'])
+        payload_ids = payload.map { |hash| hash["id"] }
 
-      expect(payload.first.keys).to eq(["id", "currency", "symbol", "name", "logo_url", "status", "price", "price_date", "price_timestamp", "circulating_supply", "max_supply", "market_cap", "market_cap_dominance", "num_exchanges", "num_pairs", "num_pairs_unmapped", "first_candle", "first_trade", "first_order_book", "rank", "rank_delta", "high", "high_timestamp", "1d", "7d", "30d", "365d", "ytd"])
+        expect(payload_ids).to match_array(["BTC", "XRP", "ETH"])
+      end
     end
   end
 
-  context 'for multiple cryptocurrencies' do
-    it 'will provide the payload for the requested cryptocurrency', :vcr do
-      payload = CryptoCheck.fetch_cryptocurrencies_information(['BTC', 'XRP', 'ETH'])
+  describe '#fetch_selected_information_for_cryptocurrencies' do
+    it 'will provide the requested information for the selected crytocurrencies only', :vcr do
+      payload = CryptoCheck.fetch_selected_information_for_cryptocurrencies(crypto_currencies: ['BTC', 'XRP', 'ETH'], attributes: ["circulating_supply", "max_supply", "name", "symbol", "price"])
+
+      expect(payload.first.keys).to match_array(["id", "circulating_supply", "max_supply", "name", "symbol", "price"])
+    end
+
+    it 'will provide the requested information for only the selected crytocurrencies only', :vcr do
+      payload = CryptoCheck.fetch_selected_information_for_cryptocurrencies(crypto_currencies: ['BTC', 'XRP', 'ETH'], attributes: ["circulating_supply", "max_supply", "name", "symbol", "price"])
       payload_ids = payload.map { |hash| hash["id"] }
 
       expect(payload_ids).to match_array(["BTC", "XRP", "ETH"])
